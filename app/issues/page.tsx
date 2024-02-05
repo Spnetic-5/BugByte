ff39ac343ed8d4ba01c55dd915570460c15ab01f
Saurabh Powar <Saurabh.Powar@axefinance.com>
2024-02-05 17:03:34 +0530

    Setup Radix UI

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import {Button} from '@radix-ui/themes';
 
 const IssuesPage = () => {
   return (
-    <div>IssuesPage</div>
+    <div>
+        <h1>Issues Page</h1>
+        <Button>Click Me</Button>
+    </div>
   )
 }
 
