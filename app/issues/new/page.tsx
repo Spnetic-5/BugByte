d77d890bae142b9b7c7ec79f83db36ad88842c7c
Saurabh Powar <Saurabh.Powar@axefinance.com>
2024-02-06 13:35:06 +0530

    Implement client side validation

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,31 +1,43 @@
 "use client";
 
 import axios from "axios";
-import { TextField, Button, Callout } from "@radix-ui/themes";
+import { TextField, Button, Callout, Text } from "@radix-ui/themes";
 import { useForm, Controller } from "react-hook-form";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { useRouter } from "next/navigation";
 import { error } from "console";
 import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from "@/app/validationSchemas";
+import { z } from "zod";
 
-interface IssueForm {
-  title: string;
-  description: string;
-}
+type IssueForm = z.infer<typeof createIssueSchema>;
+
+// interface IssueForm {
+//   title: string;
+//   description: string;
+// }
 
 const NewIssue = () => {
   const router = useRouter();
-  const { register, control, handleSubmit } = useForm<IssueForm>();
-  const [error, setError] = useState('');
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
+  const [error, setError] = useState("");
 
   return (
     <div className="max-w-lg">
-      {error && <Callout.Root color='red' className="mb-2">
-        <Callout.Text>
-          {error}
-        </Callout.Text>
-        </Callout.Root>}
+      {error && (
+        <Callout.Root color="red" className="mb-2">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
       <form
         className="space-y-3"
         onSubmit={handleSubmit(async (data) => {
@@ -33,13 +45,14 @@ const NewIssue = () => {
             await axios.post("/api/issues", data);
             router.push("/issues");
           } catch (error) {
-            setError('Please fill the required fields*');
+            setError("Please fill the required fields !!!");
           }
         })}
       >
         <TextField.Root>
           <TextField.Input placeholder="Title" {...register("title")} />
         </TextField.Root>
+        {errors.title && <Text color="red" as="p">{errors.title.message}</Text>}
         <Controller
           name="description"
           control={control}
@@ -47,7 +60,9 @@ const NewIssue = () => {
             <SimpleMDE placeholder="Description" {...field} />
           )}
         />
-
+        {errors.description && (
+          <Text color="red" as="p">{errors.description.message}</Text>
+        )}
         <Button>Submit Issue</Button>
       </form>
     </div>
