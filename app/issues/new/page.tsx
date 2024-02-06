d24cfc6877aa5c331abf4211499c7a701f90396f
Saurabh Powar <Saurabh.Powar@axefinance.com>
2024-02-06 12:28:33 +0530

    Error Handling

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,23 +1,57 @@
 "use client";
 
-import { TextField, Button } from '@radix-ui/themes';
+import axios from "axios";
+import { TextField, Button, Callout } from "@radix-ui/themes";
+import { useForm, Controller } from "react-hook-form";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
+import { useRouter } from "next/navigation";
+import { error } from "console";
+import { useState } from "react";
 
-<SimpleMDE />;
-
-const NewIssue = () => {
-  return (
-    <div className='max-w-lg space-y-3'>
-        <TextField.Root>
-          <TextField.Input placeholder='Title' />
-        </TextField.Root>
-        <SimpleMDE placeholder='Description' />
-        <Button>
-          Submit Issue
-        </Button>
-    </div>
-  )
+interface IssueForm {
+  title: string;
+  description: string;
 }
 
-export default NewIssue
+const NewIssue = () => {
+  const router = useRouter();
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const [error, setError] = useState('');
+
+  return (
+    <div className="max-w-lg">
+      {error && <Callout.Root color='red' className="mb-2">
+        <Callout.Text>
+          {error}
+        </Callout.Text>
+        </Callout.Root>}
+      <form
+        className="space-y-3"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post("/api/issues", data);
+            router.push("/issues");
+          } catch (error) {
+            setError('Please fill the required fields*');
+          }
+        })}
+      >
+        <TextField.Root>
+          <TextField.Input placeholder="Title" {...register("title")} />
+        </TextField.Root>
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
+        />
+
+        <Button>Submit Issue</Button>
+      </form>
+    </div>
+  );
+};
+
+export default NewIssue;
